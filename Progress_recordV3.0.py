cd8114f0435ceda30c29ab2f114d3e1d37e069d7
CarpenterZY <34847414+pythoncao@users.noreply.github.com>
2018-01-16 23:25:51 +0800

    Update Progress_recordV3.0.py
    normal version

--- Progress_recordV3.0.py
+++ Progress_recordV3.0.py
@@ -1,9 +1,9 @@
 import time
-x = open('D:\\recordtimes.txt')
+x = open(recordtimes.txt')
 num = int(x.read(-1)[::-1][:2][::-1])
 t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 #调用当前时间
-f = open('C:\\Users\\1468\\Desktop\\progress.txt','a',encoding='utf-8')
+f = open('progress.txt','a',encoding='utf-8')
 f.write('\n')
 f.write(str(t))
 f.write('\n')
@@ -11,7 +11,7 @@ zifu = ['看到第%d个视频 '%num]
 f.write(str(zifu))
 f.write('\n')
 num += 1
-m = open('D:\\recordtimes.txt','a')
+m = open('recordtimes.txt','a')
 m.write(str(num))
 f.write('\n')
 f.close()
